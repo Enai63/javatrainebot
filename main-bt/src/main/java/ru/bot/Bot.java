bc8f36b8cee08f2fd762f46f613d58a7d7b4e5e3
Enai63 <Toha76@yandex.ru>
2022-10-02 09:52:58 +0400

    Add class Bot

---
+++ main-bt/src/main/java/ru/bot/Bot.java
@@ -0,0 +1,32 @@
+package ru.bot;
+
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.methods.updates.SetWebhook;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.starter.SpringWebhookBot;
+
+public class Bot extends SpringWebhookBot {
+    public Bot(SetWebhook setWebhook) {
+        super(setWebhook);
+    }
+
+    @Override
+    public String getBotUsername() {
+        return null;
+    }
+
+    @Override
+    public String getBotToken() {
+        return null;
+    }
+
+    @Override
+    public BotApiMethod<?> onWebhookUpdateReceived(Update update) {
+        return null;
+    }
+
+    @Override
+    public String getBotPath() {
+        return null;
+    }
+}
